45327cb5198528dcfe6d464ce2e5f319d3ca516a
Rafael Martins <rafael.chagas9@hotmail.com>
2023-06-18 17:23:24 -0300

    First Commit

--- CustomLanguage.g4
+++ CustomLanguage.g4
@@ -1,6 +1,6 @@
 grammar CustomLanguage;
 
-@header{
+@header {
 import org.antlr.v4.runtime.*;
 import java.util.*;
 }
@@ -8,54 +8,32 @@ import java.util.*;
 // Ações semânticas
 @members {
   Pele pele = new Pele();
-
-  // Tabela de símbolos para armazenar variáveis declaradas
-    Map<String, String> symbolTable = new HashMap<>();
-
-    // Verificar se uma variável foi declarada
-    boolean isDeclared(String variable) {
-        return symbolTable.containsKey(variable);
-    }
-
-    // Verificar se uma variável pode ser usada de acordo com o escopo
-    boolean isVariableUsable(String variable) {
-        // Lógica para verificar o escopo das variáveis
-        return true;
-    }
-
-    // Declarar uma variável
-    void declareVariable(String variable, String type) {
-        symbolTable.put(variable, type);
-    }
 }
 
 // Definição das regras sintáticas
-program: start statement* end {regras.writeCode();};
+program: start statement* end {pele.writeCode();};
 
-statement: assignment | ifStatement | loopStatement | scanfStatement | printStatement;
+start: 'inizio' {pele.printInicio();};
 
-assignment returns [String type] 
-    : t=typeDeclaration ID '=' e=expression ';' 
-      {
-        if (symbolTable.containsKey($ID.text)) {
-          String varType = symbolTable.get($ID.text);
-          if (varType.equals($t.text)) {
-            symbolTable.put($ID.text, $t.text);
-            $type = $t.text;
-          } else {
-            throw new RuntimeException("Type mismatch for variable " + $ID.text);
-          }
-        } else {
-          declareVariable($ID.text, $t.text);
-          $type = $t.text;
-        }
-      };
+statement:
+	assignment
+	| ifStatement
+	| loopStatement
+	| scanfStatement
+	| printStatement;
 
-start: 'inicio' {regras.printInicio();};
+// The method that assigns variables is called declararVariavel and it takes the following parameters:
+// 1. The name of the variable
+// 2. The value of the variable
+// 3. The type of the variable (int, float, string, bool)
 
-end: 'fim' {regras.printFim();};
+assignment: type=('int' | 'float' | 'string' | 'bool') id=ID '=' (value = INT | value = DECIMAL | value = STRING | value = BOOL | value=expression) ';' {pele.declararVariavel($id.getText(), $value.getText(), $type.getText());};
 
-typeDeclaration: 'int' | 'float' | 'string' | 'bool';
+expression: atom (op=('*' | '/' | '+' | '-') atom)*;
+
+atom: ID | INT | DECIMAL | STRING | BOOL | '(' expression ')';
+
+end: 'fine' {pele.printFim();};
 
 ifStatement: 'if' '(' expression ')' block ('else' block)?;
 
@@ -65,40 +43,25 @@ whileStatement: 'while' '(' expression ')' block;
 
 doWhileStatement: 'do' block 'while' '(' expression ')' ';';
 
-forStatement: 'for' '(' assignment? ';' expression? ';' assignment? ')' block;
+forStatement:
+	'for' '(' assignment? ';' expression? ';' assignment? ')' block;
 
 block: '{' statement* '}';
 
 scanfStatement: 'leggere' '(' ID ')';
 
-printStatement: 'scrivere' '(' (id=ID | str=STRING) ')' ';' {regras.printString($id != null ? $id.getText() : $str.getText());};
+printStatement:
+	'scrivere' '(' (id = ID | str = STRING) ')' ';' {pele.printString($id != null ? $id.getText() : $str.getText());
+		};
 
-expression: logicalExpression;
-
-logicalExpression: equalityExpression ((AND | OR) equalityExpression)*;
-
-equalityExpression: relationalExpression ((OPREL | OPREL) relationalExpression)*;
-
-relationalExpression: additiveExpression OPREL additiveExpression;
-
-additiveExpression: multiplicativeExpression ((ADD | SUB) multiplicativeExpression)*;
-
-multiplicativeExpression: unaryExpression ((MUL | DIV) unaryExpression)*;
-
-unaryExpression: (ADD | SUB) atomExpression | atomExpression;
-
-atomExpression: ID | (INT | DECIMAL | STRING | BOOL) | '(' expression ')';
-
-BOOL: 'vero' {pele.BOOL("vero");} 
-    | 'falso' {pele.BOOL("falso");};
-
+BOOL: 'vero' {pele.BOOL("vero");} | 'falso' {pele.BOOL("falso");};
 
 // Definição dos tokens léxicos
 ID: [a-zA-Z][a-zA-Z0-9_]*;
 INT: [0-9]+;
 DECIMAL: [0-9]+ '.' [0-9]+;
-STRING : '"' ~["\r\n]* '"';
-OPREL: '>' | '<' | '>=' | '<=' | '==' | '!=' ;
+STRING: '"' ~["\r\n]* '"';
+OPREL: '>' | '<' | '>=' | '<=' | '==' | '!=';
 WS: [ \t\r\n]+ -> skip;
 ADD: '+';
 SUB: '-';
--- Pele.java
+++ Pele.java
@@ -1,19 +1,18 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Pele {
-    // Empty constructor
 
     String code = "";
+    // Tabela de símbolos para armazenar variáveis declaradas
+    Map<String, String> symbolTable = new HashMap<>();
 
     public Pele() {
         this.code = "";
-    }
-
-    // Method to return the greeting
-    public String getGreeting() {
-        return "Hello World!";
+        this.symbolTable = new HashMap<>();
     }
     
     // Method that prints a string
@@ -33,24 +32,77 @@ public class Pele {
         code += "}\n";
     }
 
-    // Method that writes the code string to a java file
-    public void writeCode() {
-        // Open a file
-        try {
-            FileWriter fileWriter = new FileWriter("Output.java");
+    protected void declararVariavel(String nome, String valor, String tipo) {
+        // Implementação do declararVariavel de acordo com a sua definição
+        // Verifica se a variável já foi declarada
+        boolean isdeclared = checkVariableDeclared(nome);
 
-            // Always wrap FileWriter in BufferedWriter
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-            // Write the code string to the file
-            bufferedWriter.write(this.code);
-
-            // Close the file
-            bufferedWriter.close();
-        } catch (IOException ex) {
-            System.out.println("Error writing to file '" + "Output.java" + "'");
+        if (isdeclared) {
+            throw new IllegalArgumentException("Variável já declarada: " + nome);
         }
 
+        // Verifica se o valor é compatível com o tipo
+        if (valor != null) {
+            boolean isValidType = checkVariableType(valor, tipo);
+            if (!isValidType) {
+                throw new IllegalArgumentException("Valor incompatível da variável: " + nome);
+            }
+        }
+
+        // Adiciona a variavel na tabela de simbolos
+        this.symbolTable.put(valor, tipo);
+        // Adiciona a variavel no código
+        if (tipo.equals("bool")) {
+            valor = getValidJavaBoolean(valor);
+            tipo = "boolean";
+        }
+        code += tipo + " " + nome + " = " + valor + ";";
+    }
+
+    private String getValidJavaBoolean(String value) {
+        if (value.equals("vero")) {
+            return "true";
+        } else if (value.equals("falso")) {
+            return "false";
+        } else {
+            return "false";
+        }
+    }
+
+    private boolean checkVariableType(String valor, String tipo){
+        if (tipo.equals("int")) {
+            try {
+                Integer.parseInt(valor);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else if (tipo.equals("float")) {
+            try {
+                Double.parseDouble(valor);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else if (tipo.equals("string")) {
+            if (valor.charAt(0) != '"' || valor.charAt(valor.length() - 1) != '"') {
+                return false;
+            }
+        } else if (tipo.equals("bool")) {
+            if (!valor.equals("vero") && !valor.equals("falso")) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    // Método que verifica se a variável já foi declarada
+    private boolean checkVariableDeclared(String nome) {
+        if (!this.symbolTable.containsKey(nome)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     // Method that verify if true or false
@@ -159,9 +211,36 @@ public class Pele {
     
     private boolean matchID() {
         // Implementação do matchID de acordo com a sua definição
+        return true;
     }
     
     private boolean matchTerminal(String terminal) {
         // Implementação do matchTerminal de acordo com a sua definição
+        return true;
+    }
+
+    private void grammarError() {
+        // Método responsável por tratar erros na gramática e lançar exceções
+
+    }
+
+    // Method that writes the code string to a java file
+    public void writeCode() {
+        // Open a file
+        try {
+            FileWriter fileWriter = new FileWriter("Output.java");
+
+            // Always wrap FileWriter in BufferedWriter
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Write the code string to the file
+            bufferedWriter.write(this.code);
+
+            // Close the file
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Error writing to file '" + "Output.java" + "'");
+        }
+
     }
 }
